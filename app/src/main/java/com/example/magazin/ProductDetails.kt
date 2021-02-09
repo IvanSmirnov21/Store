c812446bfb9aa0d9e0fa2aa6f27dc812be0d686f
Ivan Smirnov <89264148222@yandex.ru>
2021-02-09 14:57:38 +0300

    Разработка меню 6

--- app/src/main/java/com/example/magazin/ProductDetails.kt
+++ app/src/main/java/com/example/magazin/ProductDetails.kt
@@ -1,7 +1,9 @@
 package com.example.magazin
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_details.*
 
@@ -11,5 +13,12 @@ class ProductDetails : AppCompatActivity() {
         setContentView(R.layout.product_details)
         val title = intent.getStringExtra("title") // getStringExtra принимает намерение с ключем title
         product_name.text = title  // описание продукта TextView = намерению
+        Availability.setOnClickListener {
+            AlertDialog.Builder (this)
+                    .setMessage("В наличии")
+                    .setPositiveButton("OK") { dialog, which ->  }.create().show()
+        }
+
+
     }
     }
