59339d473b717b6425dcf5884f7a99d2cf538206
Ivan Smirnov <89264148222@yandex.ru>
2021-02-20 03:48:04 +0300

    Тесты RecycleView3

--- app/src/main/java/com/example/magazin/ProductSpecification.kt
+++ app/src/main/java/com/example/magazin/ProductSpecification.kt
@@ -3,11 +3,13 @@ package com.example.magazin
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.product_description.*
 
 class ProductSpecification: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        setContentView(R.layout.product_description)
         val title = intent.getStringExtra("title2")
+        textView2.text = title
     }
     }
